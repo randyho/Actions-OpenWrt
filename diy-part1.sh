10232ce919dd81393f9cdaef440108e6b8e51f15
Randy <randyho@qq.com>
2021-09-15 18:46:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+
+# Add luci-theme-argon
+rm -rf lede/package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git
