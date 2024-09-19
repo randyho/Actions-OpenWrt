1793977bce84f2443fc16c0b80f75ff041a9ec99
Randy <randyho@outlook.com>
2024-09-19 20:43:50 +0800

    Update diy-part1_openwrt.sh

--- diy-part1_openwrt.sh
+++ diy-part1_openwrt.sh
@@ -30,3 +30,11 @@ git clone --depth 1 -b main https://github.com/morytyann/OpenWrt-mihomo.git pack
 # git clone --depth 1 https://github.com/randyho/luci-app-clash-lite.git package/luci-app-clash-lite
 # git clone --depth 1 https://github.com/randyho/luci-app-openclash.git package/luci-app-openclash
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
