9ac16d3fae1b2b1ae09b79892f7bca6fd1914c98
Randy <randyho@outlook.com>
2024-10-16 20:10:54 +0800

    Update diy-part1_openwrt.sh

--- diy-part1_openwrt.sh
+++ diy-part1_openwrt.sh
@@ -10,13 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Add a feed source
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-# sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-# sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
-
 # Add luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
@@ -28,11 +21,3 @@ git clone --depth 1 -b main https://github.com/morytyann/OpenWrt-mihomo.git pack
 # git clone --depth 1 https://github.com/randyho/luci-app-clash-lite.git package/luci-app-clash-lite
 # git clone --depth 1 https://github.com/randyho/luci-app-openclash.git package/luci-app-openclash
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
